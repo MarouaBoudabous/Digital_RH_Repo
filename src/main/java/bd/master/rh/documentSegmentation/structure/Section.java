77088bc66322ecbc92759b62383b8a63ece7cde2
mboudabous <mboudabous@PAR-N-PW07-4712.BETD.FR>
2017-08-11 15:52:28 +0200

    Section class

---
+++ src/main/java/bd/master/rh/documentSegmentation/structure/Section.java
@@ -0,0 +1,24 @@
+package bd.master.rh.documentSegmentation.structure;
+
+public class Section {
+
+	public String title;
+	public String content;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content; 
+	}
+
+}
